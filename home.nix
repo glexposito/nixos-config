ad9fd2c75b324cfa50de27193ee0a278aa090b64
Guillermo <glexposito@gmail.com>
2026-06-21 17:24:59 +1200

    feat(terminals): add configured foot terminal and ghostty opacity

--- home.nix
+++ home.nix
@@ -37,9 +37,40 @@
   programs.ghostty = {
     enable = true;
     settings = {
+      background-opacity = 0.95;
+      background-opacity-cells = true;
       window-padding-x = 10;
       maximize = true;
       gtk-single-instance = true;
+    };
+  };
+
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        include = "${pkgs.foot.src}/themes/catppuccin-mocha";
+        term = "xterm-256color";
+        font = "JetBrainsMono Nerd Font:pixelsize=14";
+        pad = "10x0";
+        dpi-aware = "yes";
+        initial-window-mode = "maximized";
+      };
+
+      scrollback = {
+        indicator-position = "none";
+      };
+
+      colors-dark = {
+        alpha = 0.95;
+      };
+
+      csd = {
+        preferred = "none";
+        size = 0;
+        border-width = 0;
+        hide-when-maximized = "yes";
+      };
     };
   };
 
