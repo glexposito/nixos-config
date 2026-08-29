eb42c9421cf29aa6d388055d04cae6cd21ec55f0
Guillermo <glexposito@gmail.com>
2026-08-30 09:02:46 +1200

    fix(desktop): drop tuigreet user-menu, unreliable at login

--- modules/desktop/greetd.nix
+++ modules/desktop/greetd.nix
@@ -11,9 +11,6 @@
           "--time"
           "--remember" "--remember-session"
           "--asterisks"
-          "--user-menu"
-          "--user-menu-min-uid 1000"
-          "--user-menu-max-uid 29999"
           "--background doom"
           "--sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions:${config.services.displayManager.sessionData.desktops}/share/xsessions"
         ];
